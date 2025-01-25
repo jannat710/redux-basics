aa2fdce8eb39ed3ce2df2e2ff62486d8a53d2ab5
e <jannatul0040@gmail.com>
2025-01-25 15:04:27 +0600

    Implemented dark mode using shadcn

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,5 +1,5 @@
 import App from "@/App";
-import Login from "@/pages/login";
+import Login from "@/pages/Login";
 import Task from "@/pages/Task";
 import Users from "@/pages/Users";
 import { createBrowserRouter } from "react-router-dom";
