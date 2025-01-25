f888e73ac20c47b384c1efd9b59e6d26f2098df8
e <jannatul0040@gmail.com>
2025-01-26 00:49:01 +0600

    Added shadcn modal and form

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/components/module/AddTaskModal";
 import TaskCard from "@/components/module/TaskCard";
 import { selectTasks } from "@/redux/features/task/taskSlice";
 import { useAppSelector } from "@/redux/hook";
@@ -10,12 +11,13 @@ const Task = () => {
 
   return (
     <div className="mx-auto max-w-7xl px-5 mt-20">
-      <div>
+      <div className="flex justify-between items-center">
         <h1>Tasks</h1>
+        <AddTaskModal />
       </div>
       <div className="space-y-5 mt-5">
         {tasks.map((task) => (
-          <TaskCard task={task} />
+          <TaskCard key={task.id} task={task} />
         ))}
       </div>
     </div>
